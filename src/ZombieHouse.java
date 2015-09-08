4558ff3fd2cf5dae8303f42f9aec7eecdaf68c6c
Javier C <youracow@gmail.com>
2015-09-08 15:15:20 -0600

    added main

---
+++ src/ZombieHouse.java
@@ -0,0 +1,28 @@
+
+
+/**
+ *
+ * Enterence of the program.
+ */
+
+
+import javax.swing.*;
+
+public class ZombieHouse
+{
+  public static void main (String[] args)
+  {
+
+    SwingUtilities.invokeLater(() -> {
+//
+//      MainFrame frame = new MainFrame();
+//
+//      frame.setVisible(true);
+//      frame.setSize(700, 700);
+//      frame.setLocationRelativeTo(null);
+//      frame.setTitle("Conway Game of Life");
+//      frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    });
+
+  }
+}

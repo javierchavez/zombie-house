0f6ef80d50a083accd2e2e1428453051f7543b9d
Javier C <youracow@gmail.com>
2015-09-08 20:27:38 -0600

    fixed null pointer, so that we can at least run it

--- src/ZombieHouse.java
+++ src/ZombieHouse.java
@@ -12,7 +12,7 @@ import java.awt.image.BufferedImage;
 public class ZombieHouse extends JFrame implements Runnable
 {
   private ZombieHouseGame game = new ZombieHouseGame();
-  private JPanel gamePanel = new JPanel();
+  private JPanel gamePanel = new JPanel(true);
   private BufferedImage screen;
   private Graphics panelGraphics;
   private Graphics volatileGraphics;
@@ -30,12 +30,16 @@ public class ZombieHouse extends JFrame implements Runnable
   @Override
   public void run ()
   {
+    init();
+
     while (true)
     {
+      volatileGraphics.setColor(Color.black);
+      volatileGraphics.fillRect(0, 0, 1920, 1080);
 
       game.render(volatileGraphics);
 
-      panelGraphics.drawImage(screen, 0, 0, null);
+      panelGraphics.drawImage(screen, 0, 0, this);
 
 
       if (!isActive()) {
@@ -52,6 +56,10 @@ public class ZombieHouse extends JFrame implements Runnable
 
   private void init(){
     // create a jPanel and add it to this frame
+    setVisible(true);
+    setSize(1920, 1080);
+    setLocationRelativeTo(null); //center on screen
+    add(gamePanel);
 
     // image
     screen = new BufferedImage(1920, 1080, BufferedImage.TYPE_INT_ARGB);

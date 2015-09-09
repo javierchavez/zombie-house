e1ffccedab40abd69860a3afdb9274faad07af63
Javier C <youracow@gmail.com>
2015-09-09 12:01:17 -0600

    Updated defaults, sp. error board

--- src/model/House.java
+++ src/model/House.java
@@ -8,17 +8,17 @@ import java.util.stream.Collectors;
 public class House
 {
 
-  public static final int WIDTH = 11;
-  public static final int HEIGHT = 11;
+  public static final int WIDTH = 80;
+  public static final int HEIGHT = 80;
 
-  private List<List<Object>> boerd = new ArrayList<>();
+  private List<List<Object>> board = new ArrayList<>();
   private List<Tile> tiles = new ArrayList<>();
   private List<Zombie> zombies = new ArrayList<>();
   private Character player;
 
   public House (Character player)
   {
-
+    this.player = player;
   }
 
 

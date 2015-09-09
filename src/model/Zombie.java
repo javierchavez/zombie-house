81709e2780086b4e2a51da873d44b75f1ffea064
Javier C <youracow@gmail.com>
2015-09-08 21:57:20 -0600

    implemented class for taking life

--- src/model/Zombie.java
+++ src/model/Zombie.java
@@ -3,12 +3,24 @@ package model;
 
 import java.util.List;
 
-public class Zombie implements Path
+public class Zombie implements Path, LifeTaker
 {
   @Override
   public List<Object> find (House board, Object start, Object end)
   {
     // since this is Zombie we can use the BFS or Dijkstra alg
     return null;
+  }
+
+  @Override
+  public void setTakePoints (float points)
+  {
+
+  }
+
+  @Override
+  public float getTakePoints ()
+  {
+    return 0;
   }
 }

66f8fdefd78c40488e1258f2f162e6d320203c55
Javier C <youracow@gmail.com>
2015-09-10 21:25:15 -0600

    @alexebaker I put the tile defaults in here

--- src/view/Renderer.java
+++ src/view/Renderer.java
@@ -4,5 +4,13 @@ import java.awt.*;
 
 public interface Renderer
 {
+  int TILE_WIDTH = 80;
+  int TILE_HEIGHT = 80;
+  float displayScaleFactor = 1.5f;
+  int MAX_TILE_SIZE = 80;
+  int MAX_SCREEN_WIDTH = 1920;
+  int MAX_SCREEN_HEIGHT = 1080;
+
+
   void render(Graphics g);
 }

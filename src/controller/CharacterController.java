de7c6bca183eb682e1ccf8fed67e15a702879688
esosebee <esosebee@unm.edu>
2015-09-11 15:53:48 -0600

    Set up methods/basic and incomplete logic in CharacterController just to get started

--- src/controller/CharacterController.java
+++ src/controller/CharacterController.java
@@ -6,8 +6,15 @@ import model.House;
 
 public class CharacterController
 {
-
   private final House house;
+  private boolean isMoving = false;
+  private int direction;
+
+  ///////////////////////////////////////////////////////////////////////////////////////////
+  // Placeholder variables until I know how to get this information from the other classes //
+  ///////////////////////////////////////////////////////////////////////////////////////////
+  private boolean pickup = false;
+  private int traps = 0;
 
   public CharacterController (House house)
   {
@@ -30,10 +37,70 @@ public class CharacterController
     // change the player's x and y
     // the distance of the player is dependant on time...
     // the player's x and y are in Pixels
+
+    if (isMoving)
+    {
+      // TODO: do stuff
+      // playerSpeed * deltaTime determines how many tiles/second player will move across??
+      player.setRotation(direction);
+      isMoving = false;
+    }
   }
 
-  public void pickup ()
+  /**
+   * To pick up a trap.
+   */
+  public void pickup()
   {
-
+    if (pickup)
+    {
+      // pick up trap
+      traps++;
+    }
   }
+
+  /**
+   * To set a trap.
+   */
+  public void setTrap()
+  {
+    if (!pickup)
+    {
+      // set trap
+      traps--;
+    }
+  }
+
+  /**
+   * If w or up arrow is pressed.
+   */
+  public void moveUp()
+  {
+    direction = 90;
+  }
+
+  /**
+   * If a or left arrow is pressed.
+   */
+  public void moveLeft()
+  {
+    direction = 180;
+  }
+
+  /**
+   * If s or down arrow is pressed.
+   */
+  public void moveDown()
+  {
+    direction = 270;
+  }
+
+  /**
+   * If d or right arrow is pressed.
+   */
+  public void moveRight()
+  {
+    direction = 0;
+  }
+
 }

e3dd2529222b4e721ba8ce09deea90c2b0f5e0af
Javier C <youracow@gmail.com>
2015-09-13 14:48:45 -0600

    New finder

---
+++ src/model/Line.java
@@ -0,0 +1,20 @@
+package model;
+
+
+import java.util.List;
+
+public class Line implements Finder<Tile>
+{
+
+  @Override
+  public void find (House board, Tile start, Tile end)
+  {
+
+  }
+
+  @Override
+  public List<Tile> getPath ()
+  {
+    return null;
+  }
+}
---
+++ src/model/Rand.java
@@ -0,0 +1,19 @@
+package model;
+
+
+import java.util.List;
+
+public class Rand implements Finder<Tile>
+{
+  @Override
+  public void find (House board, Tile start, Tile end)
+  {
+
+  }
+
+  @Override
+  public List<Tile> getPath ()
+  {
+    return null;
+  }
+}

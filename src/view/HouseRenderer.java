6380c26cc1461f0edc9e03b0a451ad674b487e5a
Javier C <youracow@gmail.com>
2015-09-16 10:11:09 -0600

    rendering fire trap to house

--- src/view/HouseRenderer.java
+++ src/view/HouseRenderer.java
@@ -15,15 +15,17 @@ public class HouseRenderer extends Renderer
   private final House house;
   FloorGraphic floor = new FloorGraphic();
   WallGraphic wall = new WallGraphic();
+  TrapGraphic trap = new TrapGraphic();
 
   public HouseRenderer (House house, Converter converter)
   {
     super(house.getPlayerTile().getX(),house.getPlayerTile().getY(), converter);
     this.house = house;
+    // center the renderer on the player tile
   }
 
   @Override
-  public void render (java.awt.Graphics g)
+  public void render (Graphics g)
   {
     Graphics2D g2 = (Graphics2D)g;
 
@@ -33,19 +35,19 @@ public class HouseRenderer extends Renderer
     double width = g2.getClipBounds().getWidth();
     double height = g2.getClipBounds().getHeight();
 
-    int tileW = (int) (width / house.getWidth());
-    int tileH = (int) (height / house.getHeight());
 
 
-    // int cellsX = (int) Math.ceil(g2.getClipBounds().getWidth()/80f);
-    // int cellsY = (int) Math.ceil(g2.getClipBounds().getHeight()/80f);
+    int cellsX = (int) Math.ceil(g2.getClipBounds().getWidth()/80f);
+    int cellsY = (int) Math.ceil(g2.getClipBounds().getHeight()/80f);
+    int tileW = (int) (width / cellsX);
+    int tileH = (int) (height / cellsY);
     ////////// end shit code ///////////
 
     Tile[][] houseMatrix = house.getHouse();
 
-    for (int i = 0; i < houseMatrix.length; i++)
+    for (int i = 0; i < cellsY; i++)
     {
-      for (int j = 0; j < houseMatrix[0].length; j++)
+      for (int j = 0; j < cellsX; j++)
       {
         if (houseMatrix[i][j] instanceof Wall)
         {
@@ -56,7 +58,14 @@ public class HouseRenderer extends Renderer
         {
           g2.drawImage(floor.getImage(), j * tileW,
                        i * tileH, null);
+          if(houseMatrix[i][j].getTrap() == Tile.Trap.FIRE)
+          {
+            g2.drawImage(trap.getImage(), (j * tileW)+20,
+                         (i * tileH)+20, null);
+
+          }
         }
+
       }
     }
   }

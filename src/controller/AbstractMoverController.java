5ab33c513ff5c4325d7fe40a8e4aab68035f050b
Javier C <youracow@gmail.com>
2015-09-18 22:17:50 -0600

    Refactoring into a more polymorphic approach to reduce coding

---
+++ src/controller/AbstractMoverController.java
@@ -0,0 +1,149 @@
+package controller;
+
+
+import model.House;
+import model.Mover;
+
+/**
+ * This class does most of the work for setting the character speed and rotation
+ * @param <T> type of mover you are wanting to move... it must extend Mover
+ *           in the model.
+ */
+public abstract class AbstractMoverController<T extends Mover> implements MoverController<T>
+{
+
+  protected static House house;
+  protected T mover;
+  protected boolean isMoving = false, running = false, idling = true;
+  protected boolean moveUp, moveDown, moveLeft, moveRight;
+  private boolean DEBUG = false;
+
+  public AbstractMoverController (House house, T mover)
+  {
+    AbstractMoverController.house = house;
+    this.mover = mover;
+  }
+
+  public AbstractMoverController (House house)
+  {
+    this(house, null);
+  }
+
+  @Override
+  public void run ()
+  {
+    isMoving = true;
+    if (mover.getStamina() > 0) running = true;
+  }
+
+  @Override
+  public void walk ()
+  {
+    isMoving = true;
+    running = false;
+    idling = false;
+  }
+
+  @Override
+  public void idle ()
+  {
+    // if (DEBUG) System.out.println("Idling...");
+    idling = true;
+    isMoving = false;
+    running = false;
+    mover.setSpeed(Mover.IDLE);
+  }
+
+  @Override
+  public void stopMoving() // TODO: get rid of this method later? probably don't need it
+  {
+    System.out.println("I can't move like that!");
+    isMoving = false;
+    running = false;
+    mover.setSpeed(Mover.IDLE);
+  }
+
+  @Override
+  public void moveUp ()
+  {
+    if (DEBUG) System.out.println("Moving up");
+    isMoving = true;
+    idling = false;
+    moveUp = true;
+    //direction = Mover.NORTH; // Change mover's direction
+    mover.setRotation(Mover.NORTH);
+  }
+
+  @Override
+  public void moveDown ()
+  {
+    if (DEBUG) System.out.println("Moving down");
+    isMoving = true;
+    idling = false;
+    moveDown = true;
+    //direction = Mover.SOUTH;
+    mover.setRotation(Mover.SOUTH);
+  }
+
+  @Override
+  public void moveLeft ()
+  {
+    if (DEBUG) System.out.println("Moving left");
+    isMoving = true;
+    idling = false;
+    moveLeft = true;
+    //direction = Mover.WEST;
+    mover.setRotation(Mover.WEST);
+  }
+
+  @Override
+  public void moveRight ()
+  {
+    if (DEBUG) System.out.println("Moving right");
+    isMoving = true;
+    idling = false;
+    moveRight = true;
+    //direction = Mover.EAST;
+    mover.setRotation(Mover.EAST);
+  }
+  @Override
+  public void moveUpRight()
+  {
+    if (DEBUG) System.out.println("Moving up right");
+    isMoving = true;
+    idling = false;
+    //direction = NORTHEAST;
+    mover.setRotation(Mover.NORTHEAST);
+  }
+  @Override
+  public void moveUpLeft()
+  {
+    isMoving = true;
+    idling = false;
+    //direction = NORTHWEST;
+    mover.setRotation(Mover.NORTHWEST);
+  }
+  @Override
+  public void moveDownRight()
+  {
+    isMoving = true;
+    idling = false;
+    //direction = SOUTHEAST;
+    mover.setRotation(Mover.SOUTHEAST);
+  }
+  @Override
+  public void moveDownLeft()
+  {
+    isMoving = true;
+    idling = false;
+    //direction = SOUTHWEST;
+    mover.setRotation(Mover.SOUTHWEST);
+  }
+
+  @Override
+  public void resting()
+  {
+    if (DEBUG) System.out.println("\tResting...");
+    idling = true;
+  }
+}
---
+++ src/controller/MoverController.java
@@ -0,0 +1,63 @@
+package controller;
+
+
+import model.*;
+
+interface MoverController <T extends Mover>
+{
+
+
+
+  void checkCollision (float deltaTime);
+
+
+  void update (float deltaTime);
+  /**
+   * If 'R' is pressed, player's speed is updated to 2.0, making them two times faster.
+   */
+  void run ();
+  /**
+   * When 'R' is released, player's speed drops back down to 1.0, the default speed.
+   */
+  void walk ();
+  /**
+   * When the  isn't moving at all
+   */
+  void idle ();
+  /**
+   * If two incompatible directions are pressed;
+   */
+  void stopMoving();
+  /**
+   * If 'P' is pressed.
+   */
+//  void trapInteraction ();
+
+  /**
+   * If 'W' or up arrow is pressed.
+   */
+  void moveUp ();
+  /**
+   * If 'S' or down arrow is pressed.
+   */
+  void moveDown ();
+  /**
+   * If 'A' or left arrow is pressed.
+   */
+  void moveLeft ();
+  /**
+   * If 'D' or right arrow is pressed.
+   */
+
+  void moveRight ();
+
+  void moveUpRight();
+
+  void moveUpLeft();
+
+  void moveDownRight();
+
+  void moveDownLeft();
+
+  void resting();
+}

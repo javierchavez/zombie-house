df236fbe5159c7e8a8f4f73289855d821e923e56
Javier C <youracow@gmail.com>
2015-09-20 19:47:12 -0600

    added image string

--- src/view/FireSprite.java
+++ src/view/FireSprite.java
@@ -1,6 +1,12 @@
 package view;
 
 
+import java.awt.image.BufferedImage;
+
 public class FireSprite
 {
+  public static BufferedImage getSprite (int x, int y)
+  {
+    return Sprite.getSprite("explosion-sprite.png", x, y);
+  }
 }

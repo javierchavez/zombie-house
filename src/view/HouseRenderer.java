4f77ab4fe23c2e70676d670761f614cb30a4bbc4
Javier C <youracow@gmail.com>
2015-09-24 00:24:53 -0600

    Drawing only burned state

--- src/view/HouseRenderer.java
+++ src/view/HouseRenderer.java
@@ -35,43 +35,48 @@ public class HouseRenderer extends Renderer
     {
       for (int j = 0; j < houseMatrix[i].length; j++)
       {
+        if (houseMatrix[i][j].getTrap() == Trap.BURNED)
+        {
+          g2.drawImage(trap.getImageBurned(), (j * tileW), (i * tileH), null);
+          continue;
+        }
+
         if (houseMatrix[i][j] instanceof Wall)
         {
           g2.drawImage(wall.getImage(), j * tileW, i * tileH, null);
-
         }
         else if (houseMatrix[i][j] instanceof Floor)
         {
           g2.drawImage(floor.getImage(), j * tileW, i * tileH, null);
-          if(houseMatrix[i][j].getTrap() == Trap.FIRE)
+          if (houseMatrix[i][j].getTrap() == Trap.FIRE)
           {
-            g2.drawImage(trap.getImage(), (j * tileW)+20, (i * tileH)+20, null);
+            g2.drawImage(trap.getImage(),
+                         (j * tileW) + 20,
+                         (i * tileH) + 20, null);
 
-          }
-          else if (houseMatrix[i][j].getTrap() == Trap.ACTIVATED)
-          {
-            g2.drawImage(trap.getImageBurned(), (j * tileW), (i * tileH), null);
           }
         }
         else if (houseMatrix[i][j] instanceof Obstacle)
         {
           // g2.drawImage(floor.getImage(), j * tileW, i * tileH, null);
-          g2.drawRect(j * tileW, i * tileH, 80,80);
+          g2.drawRect(j * tileW, i * tileH, 80, 80);
         }
         else if (houseMatrix[i][j] instanceof Exit)
         {
           // g2.drawImage(floor.getImage(), j * tileW, i * tileH, null);
           g2.drawImage(exit.getImage(), j * tileW, i * tileH, null);
         }
-//        g2.setColor(Color.green);
-//
-//        if (j == houseMatrix[i].length - 1 ||
-//                i == houseMatrix.length - 1 ||
-//                j == 0 || i == 0)
-//        {
-//          g2.setColor(Color.red);
-//        }
-//        g2.drawRect(j * tileW, i * tileH, 80, 80);
+
+
+        //        g2.setColor(Color.green);
+        //
+        //        if (j == houseMatrix[i].length - 1 ||
+        //                i == houseMatrix.length - 1 ||
+        //                j == 0 || i == 0)
+        //        {
+        //          g2.setColor(Color.red);
+        //        }
+        //        g2.drawRect(j * tileW, i * tileH, 80, 80);
       }
     }
   }
